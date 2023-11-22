acaa1294831bc6600f98a65580c5974715f139d7
FabioDesktop <12300113@student.pxl.be>
2023-11-22 23:21:22 +0100

    COOKIE-13.2: Aanpassing gemaakt zodat met 1 clickhandler alle knoppen aangeroepen kunnen worden. Klein probleem nog met de aankopen, als we op oma klikken na een aantal cursors te kopen, krijgt oma het volgende aankoop aantal wat zou moeten volgen op cursor vb:3 cursors en 0 oma, klik op aankoop oma, oma wordt 4. Deze morgen verder oplossen en uitpluizen.

--- CookieCliker/CookieCliker/MainWindow.xaml.cs
+++ CookieCliker/CookieCliker/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -23,6 +24,10 @@ namespace CookieCliker
 
         double teller = 0;
         double clicker = 0;
+        Button winkelButton = new Button();
+        Label labelPrijs = new Label();
+        Label labelAantKlik = new Label();
+
 
         public MainWindow()
         {
@@ -41,86 +46,174 @@ namespace CookieCliker
 
         private void ImgCookie_MouseUp(object sender, MouseButtonEventArgs e)
         {
+
             ImgCookie.Width = ImgCookie.ActualWidth + 10;
 
-            teller++;
-
+            teller++;            
+            
             UpdateScore();
             UpdateTitle();
-            VisibleButton();
-           
+            //VisibleButton();
+            
             
         }
 
+        /// <summary>
+        /// Zorgt voor de update van de score boven het koekje. 
+        /// </summary>
+        /// <returns></returns>
         private int UpdateScore()
         {
             LblScore.Content = Math.Round(teller).ToString();
             int score = Convert.ToInt32(LblScore.Content);
-            if (score >= Convert.ToDouble(LblPrijs1.Content))
-            {
-                BtnStore1.IsEnabled = true;
-            }
-            else
-                BtnStore1.IsEnabled = false;
+            
 
             return score;
         }
-
+        /// <summary>
+        /// Update de score in titelbalk en zorgt voor de zichtbaarheid van de aankoopknoppen.
+        /// </summary>
         private void UpdateTitle()
         {
             string titleScore = Convert.ToString(UpdateScore());
             Title = $"{titleScore} cookies  -  Cookie Clicker";
+
+            ShopButtonVisible();
         }
 
         private void VisibleButton()
         {
-            if (UpdateScore() == 60000)
+            if (UpdateScore() >= 60000)
             {
                 BtnStore5.Visibility = Visibility.Visible;
             }
 
-        }
-
-        //private void ButtonEnable()
-        //{
-        //    if (UpdateScore() >= Convert.ToDouble(LblPrijs1.Content))
-        //    {
-        //        BtnStore1.IsEnabled = true;
-        //    }
-        //    else
-        //        BtnStore1.IsEnabled = false;
-               
-        //    //do
-        //    //{
-        //    //    BtnStore1.IsEnabled = true;
-        //    //} while (UpdateScore() >= Convert.ToDouble(LblPrijs1.Content));
-        //}
-
-        private void BtnStore1_Click(object sender, RoutedEventArgs e)
-        {
-            double aankoopprijs = AankoopCursor();
-            AftrekAankoop(aankoopprijs);
-            clicker++;
-            LblAantalKlik1.Content = clicker.ToString();           
-            
-        }
-
-        private double AankoopCursor()
+        }     
+       
+        /// <summary>
+        /// Per aankoop wordt gekeken hoeveel maal het al aangekocht is geweest.
+        /// Indien aankopen 0 zijn, wordt eerst de basisprijs gehanteerd die hard coded staat.
+        /// Zijn er al aankopen gebeurd, wordt de basisprijs (hardcoded) vermedigvuldigd met 1.15 tot de macht van aantal aankopen.
+        /// de aankoopprijs wordt afgerond weergegeven in het spel.
+        /// </summary>
+        /// <returns></returns>
+        private double AankoopStore()
         {            
-            double basisCursor = Convert.ToDouble(LblPrijs1.Content);
-            double aankoopprijs = basisCursor * Math.Pow(1.15, Convert.ToDouble(LblAantalKlik1.Content));
+            double basisPrijs = Convert.ToDouble(labelPrijs.Content);
+            double aankoopprijs = (Convert.ToDouble(labelPrijs.Content));
+            if (Convert.ToDouble(LblAantalKlik1.Content) == 0)
+            {
+                aankoopprijs = (Convert.ToDouble(labelPrijs.Content));
+                teller -= aankoopprijs;
+                UpdateScore();
+                UpdateTitle();
+                aankoopprijs = basisPrijs * 1.15;
+            }
+            else if (UpdateScore() >= (Convert.ToDouble(labelPrijs.Content)))
+            {               
+                teller -= aankoopprijs;
+                UpdateScore();
+                UpdateTitle();
+                aankoopprijs = basisPrijs * Math.Pow(1.15, Convert.ToDouble(labelAantKlik.Content));
+            }
+            
             aankoopprijs = Math.Round(aankoopprijs);
 
-            LblPrijs1.Content = aankoopprijs.ToString();
+            labelPrijs.Content = aankoopprijs.ToString();
 
             return aankoopprijs;
         }
 
-        private void AftrekAankoop(double aankoopprijs)
+        private void Btn_Click(object sender, EventArgs e)
         {
-            teller -= aankoopprijs;
-            UpdateScore();
-            UpdateTitle();
+            string buttonName = ((Button)sender).Name;
+            string aankoop = buttonName.Substring(buttonName.Length - 1, 1);
+
+            StoreButton(aankoop);
+            AankoopStore();
+            //clicker.Equals(labelAantKlik);
+            clicker++;
+            labelAantKlik.Content = clicker.ToString();
+            
+        }
+       
+
+        private void StoreButton(string button)
+        {
+            switch(button)
+            {
+                case "1":
+                    winkelButton = BtnStore1;
+                    labelPrijs = LblPrijs1;
+                    labelAantKlik = LblAantalKlik1;
+                break;
+                case "2":
+                    winkelButton = BtnStore2;
+                    labelPrijs = LblPrijs2;
+                    labelAantKlik = LblAantalKlik2;
+                    break;
+                case "3":
+                    winkelButton = BtnStore3;
+                    labelPrijs = LblPrijs3;
+                    labelAantKlik = LblAantalKlik3;
+                    break;
+                case "4":
+                    winkelButton = BtnStore4;
+                    labelPrijs = LblPrijs4;
+                    labelAantKlik = LblAantalKlik4;
+                    break;
+                case "5":
+                    winkelButton = BtnStore5;
+                    labelPrijs = LblPrijs5;
+                    labelAantKlik = LblAantalKlik5;
+                    break;
+                
+            }
+        }
+        
+        private void ShopButtonVisible()
+        {
+            if (UpdateScore() >= Convert.ToDouble(LblPrijs1.Content))
+            {
+                BtnStore1.IsEnabled = true; 
+            }
+            else
+            {
+                BtnStore1.IsEnabled = false;
+            }
+            if (UpdateScore() >= Convert.ToDouble(LblPrijs2.Content))
+            {
+                BtnStore2.IsEnabled = true;
+            }
+            else
+            {
+                BtnStore2.IsEnabled = false;
+            }
+            if (UpdateScore() >= Convert.ToDouble(LblPrijs3.Content))
+            {
+                BtnStore3.IsEnabled = true;
+            }
+            else
+            {
+                BtnStore3.IsEnabled = false;
+            }
+            if (UpdateScore() >= Convert.ToDouble(LblPrijs4.Content))
+            {
+                BtnStore4.IsEnabled = true;
+            }
+            else
+            {
+                BtnStore4.IsEnabled = false;
+            }
+            if (UpdateScore() >= Convert.ToDouble(LblPrijs5.Content))
+            {
+                BtnStore5.IsEnabled = true;
+            }
+            else
+            {
+                BtnStore5.IsEnabled = false;
+            }
+
         }
     }
 }
